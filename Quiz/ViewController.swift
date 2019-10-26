09ad9f40b0ffc8eaeac1492a704ad98450351739
KarlaJu <karla@makingdevs.com>
2019-10-26 16:05:26 -0500

    Change contrains

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -3,15 +3,39 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet var questionLabel: UILabel!
     @IBOutlet var answerLabel: UILabel!
+    
+    let questions: [String] = [
+    "What is 7+7?",
+    "What is the capital of Vermont?",
+    "What ise cognac made from?"]
+    
+    let answers: [String] = [
+    "14",
+    "Montpelier",
+    "Grapes"]
+    
+    var currentQuestionIndex: Int = 0
+    
     @IBAction func showNextQuestion(_ sender: UIButton) {
-        questionLabel.text = "Funciona"
+        currentQuestionIndex += 1
+        if currentQuestionIndex == questions.count {
+            currentQuestionIndex = 0
+        }
+        let  question: String = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
         
     }
     @IBAction func showAnswer(_ sender: UIButton) {
-        answerLabel.text = "Funciona"
-        
+        let answer: String = answers[currentQuestionIndex]
+        answerLabel.text = answer
     }
-
+    
+    //you are providing a custom implementation for a method
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        questionLabel.text = questions[currentQuestionIndex]
+    }
 
 }
 
