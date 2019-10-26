a3f2b2947ed2912efc14d2de4909ff9d1de1adcf
KarlaJu <karla@makingdevs.com>
2019-10-26 15:46:08 -0500

    Agregando evento en los botones

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -3,10 +3,12 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet var questionLabel: UILabel!
     @IBOutlet var answerLabel: UILabel!
-    @IBAction func showNextQuestion(_sender: UIButton) {
+    @IBAction func showNextQuestion(_ sender: UIButton) {
+        questionLabel.text = "Funciona"
         
     }
     @IBAction func showAnswer(_ sender: UIButton) {
+        answerLabel.text = "Funciona"
         
     }
 
