a1f15dacd35dfb7fdb78cb45256ec5d6322422a9
KarlaJu <karla@makingdevs.com>
2019-10-26 15:41:23 -0500

    Haciendo conecciones en el interface builder

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -3,9 +3,10 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet var questionLabel: UILabel!
     @IBOutlet var answerLabel: UILabel!
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBAction func showNextQuestion(_sender: UIButton) {
+        
+    }
+    @IBAction func showAnswer(_ sender: UIButton) {
         
     }
 
